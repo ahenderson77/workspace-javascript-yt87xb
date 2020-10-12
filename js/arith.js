53c3424b955815c374dd55815eb12427da734a35
ahenderson77 <aaronmh@usca.edu>
2020-10-12 13:13:26 -0400

    1 file modified, 1 added

---
+++ js/arith.js
@@ -0,0 +1,7 @@
+// get element
+
+var num1 = parseInt(document.getElementById('n1').textContent);
+var num2 = parseInt(document.getElementById('n2').textContent);
+var c =  num1 + num2;
+
+document.write(c);
